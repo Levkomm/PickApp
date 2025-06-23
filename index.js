95833c51861e402e5279fbb0432a7872b2e988a7
Levkomm <lev.komm@gmail.com>
2025-06-23 11:59:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
+
 //copy of no zapier code
 
->>>>>>> 1e64aeb2da0581bc42245b03693f72bb9a73f4f8
-//const base44 = require('@base44/sdk');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
@@ -85,15 +82,11 @@ client.on('message', async message => {
 const processMessages = async function () {
 	const appId = '680e678ffae7fe33fb4ad6c7';
 	const apiKey	 = '25fb47e1037048e5a3b84739b433b79c';
-<<<<<<< HEAD
 
-=======
->>>>>>> 1e64aeb2da0581bc42245b03693f72bb9a73f4f8
     console.log("Calling the 'processMessages' function for app: 680e678ffae7fe33fb4ad6c7...");
   try {
     // Construct the URL to your server function
     const functionUrl = `https://base44.app/api/apps/${appId}/functions/processMessages`;
-<<<<<<< HEAD
     const headers = {
        'X-API-Key': ` ${apiKey}`,
         'Content-Type': 'application/json'
@@ -103,7 +96,6 @@ console.log(headers);
     const response = await fetch(functionUrl, {
       method: 'POST',
       headers:headers ,
-=======
     
     // Make the HTTP request with your API key
     const response = await fetch(functionUrl, {
@@ -112,7 +104,6 @@ console.log(headers);
         'Authorization': `Bearer ${apiKey}`,
         'Content-Type': 'application/json'
       },
->>>>>>> 1e64aeb2da0581bc42245b03693f72bb9a73f4f8
       body: JSON.stringify({}) // Empty body since processMessages doesn't need parameters
     });
 
@@ -150,11 +141,8 @@ const base44 = await import('@base44/sdk');
                 
                 const base44SDK = base44.createClient({
                 appId: '680e678ffae7fe33fb4ad6c7',
-<<<<<<< HEAD
 		apiKey: '25fb47e1037048e5a3b84739b433b79c',
-=======
 		token: '25fb47e1037048e5a3b84739b433b79c',
->>>>>>> 1e64aeb2da0581bc42245b03693f72bb9a73f4f8
                 });
                 // --- 3. Validation ---
                 // Ensure the required fields are present in the incoming data
